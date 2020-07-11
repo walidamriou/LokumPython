4b9405ab4535e2a55cb8b498fb6aafe6353f9e7b
Walid Amriou <walidamriou@gmail.com>
2020-07-11 23:15:31 +0200

    Update 001_Getting_Started/002_Variables_and_Data_Types/main.py

--- 001_Getting_Started/002_Variables_and_Data_Types/main.py
+++ 001_Getting_Started/002_Variables_and_Data_Types/main.py
@@ -19,72 +19,95 @@
 # Integer
 a = 2 # or by specify a the type: x = int(2)	
 print(a)
+print(' ')
 
 # Integer
 b = 9223372036854775807
 print(b)
+print(' ')
 
 # Floating point
 pi = 3.14 # or or by specify a the type: Pi = float(3.14)	
 print(pi)
+print(' ')
 
 # String (but like char)
 c = 'A'
 print(c)
+print(' ')
 
 # String
 name = 'Lokum Python' # or by specify a the type: name = str("Lokum Python")	
 print(name)
+print(' ')
 
 # 'Lokum Python' or "Lokum Python" is the same as
 name = "Lokum Python"
 print(name)
+print(' ')
 
 # Boolean
 d = True # or by specify a the type: d = bool(True)
 print(d)
+print(' ')
 
 # Empty value or null data type
 e = None
 print(e)
+print(' ')
 
 # complex
 f1=1j + 12 # or by specify a the type: x = complex(1j + 12)	
 f2=12j
 print(f1)
 print(f2)
+print(' ')
 
 # tuple
 x = ("apple", "banana", "cherry") # or by specify a the type: x = tuple(("apple", "banana", "cherry"))	
 print(x)	
 print(x[0])
+print(' ')
 
 # range
 x = range(6)	
 print(x)	
+print(' ')
+
 # dict
 x = {"name" : "walid", "age" : 25} # or by specify a the type: x = dict(name="walid", age=25)		
 print(x)
 print(x['name']) # or print(x["name"])
+print(' ')
 
 # set
 x = {"apple", "banana", "cherry"} # or by specify a the type: x = set(("apple", "banana", "cherry"))	
 print(x)
+print(' ')
 
 # frozenset
 x = frozenset({"apple", "banana", "cherry"})		
+print(' ')
 
 # bytes
 x = b"Hello" # or by specify a the type: x = bytes(b"Hello")	
+print(' ')
 
 # bytearray
 x = bytearray(5) 	
+print(' ')
 
 # memoryview
 x = memoryview(bytes(5))
+print(' ')
 
 # list
 g = ["apple", "banana", "cherry"] # or by specify a the type: x = list(("apple", "banana", "cherry"))	
+g.append("orange") # we use append() to add Items to list
+print(g)
+g.remove("banana") # we use remove() to add Items to list
+print(g)
+print(' ')
 
 #Legal variable names:
 projectname = "LokumPython"
@@ -100,6 +123,7 @@ print(_project_name)
 print(projectName)
 print(PROJECTNAME)
 print(projectname1)
+print(' ')
 
 #Illegal variable names:
 # 2projectname = "LokumPython"
@@ -112,13 +136,16 @@ x, y, z = "Oranga", "Banano", "binina" # names from my mind :D
 print(x)
 print(y)
 print(z)
+print(' ')
 
 # assign the same value to multiple variables
 x = y = z = "Oranga"
 print(x)
 print(y)
 print(z)
+print(' ')
 
 # Print with combine both text and a variable
 print("So it is "+ x)
+print(' ')
 
