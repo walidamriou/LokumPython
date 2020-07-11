f588b4dc18c409fe27b87ad7d4b3eaa1f1c702d8
Walid Amriou <walidamriou@gmail.com>
2020-07-11 13:48:36 +0200

    Create 001_Getting_Started/002_Variables/main.py

---
+++ 001_Getting_Started/002_Variables/main.py
@@ -0,0 +1,49 @@
+"""
+ ******************************************************************** 
+
+  LokumPython
+  To Learn Go Programming Language with small programs examples
+  Project Website: LokumPython.walidamriou.com
+  Github: https://github.com/walidamriou/LokumPython
+
+  Copyright CC 2020 Walid Amriou
+  This work is licensed under a Creative Commons Attribution-NonCommercial-ShareAlike 4.0 International License
+   
+  Last update: July 2020
+
+ ******************************************************************** 
+"""
+# The declaration of the variables in Python like that:  <variable name> = <value>
+
+print('LokumPython')
+
+# Integer
+a = 2
+print(a)
+
+# Integer
+b = 9223372036854775807
+print(b)
+
+# Floating point
+pi = 3.14
+print(pi)
+
+# String (but like char)
+c = 'A'
+print(c)
+
+# String
+name = 'Lokum Python'
+print(name)
+
+# Boolean
+d = True
+print(d)
+
+# Empty value or null data type
+e = None
+print(e)
+
+print(' ')
+
