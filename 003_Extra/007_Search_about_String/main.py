01928565650ab174f19d71451b0b73f5fe6e2220
Walid Amriou <walidamriou@gmail.com>
2020-07-15 01:06:12 +0200

    add 003_Extra/007_Search_about_String/main.py

---
+++ 003_Extra/007_Search_about_String/main.py
@@ -0,0 +1,16 @@
+"""
+ ******************************************************************** 
+
+  LokumPython
+  To Learn Python Programming Language with small programs examples
+  Project Website: LokumPython.walidamriou.com
+  Github: https://github.com/walidamriou/LokumPython
+
+  Copyright CC 2020 Walid Amriou
+  This work is licensed under a Creative Commons Attribution-NonCommercial-ShareAlike 4.0 International License
+   
+  Last update: July 2020
+
+ ******************************************************************** 
+"""
+
